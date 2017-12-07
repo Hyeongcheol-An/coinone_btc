c24fd94eb5acabea65f0019b63749d9e7bd4d767
Hyeongcheol-An <anh1026@kaist.ac.kr>
2017-12-08 00:58:29 +0900

    First commit coinone bitcoin API

---
+++ btc.py
@@ -0,0 +1,27 @@
+import json
+import urllib.request
+import codecs
+import threading
+from urllib.request import Request, urlopen
+from time import sleep
+
+
+def coinone():
+
+	urlTicker = urllib.request.urlopen('https://api.coinone.co.kr/trades')
+	readTicker = urlTicker.read().decode('utf8')
+	jsonTicker = json.loads(readTicker)
+	FindTIME = jsonTicker['timestamp']
+	TIME = int(FindTIME)
+	for h in jsonTicker['completeOrders']:
+		FindBTC = h['price']
+	BTC = int(FindBTC)
+	#print(TIME)
+	print(BTC, "원")
+	threading.Timer(1, coinone).start()
+	urlTicker = urllib.request.urlopen('https://api.coinone.co.kr/orderbook')
+
+
+if __name__ == "__main__":
+
+	coinone()
